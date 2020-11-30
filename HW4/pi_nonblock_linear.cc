b7771aabd89af13508002d3bb0d416af5e2397de
zonghan <steven.me05@nctu.edu.tw>
2020-11-30 21:16:30 +0800

    [HW4] finish 1.2.3, failed to run as fast as ref

--- HW4/pi_nonblock_linear.cc
+++ HW4/pi_nonblock_linear.cc
@@ -16,23 +16,54 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: MPI init
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    long long int num_task = tosses / world_size;
+    int SEED = 78;
+    int tag = 0;
+    long long int count = 0;
+
+    int seed = world_rank * SEED, dest = 0;
+    long long int local_cnt = 0;
+    srand(seed);
+    for (long long int toss = 0; toss < num_task; toss++){
+        double x = (double) rand() / RAND_MAX;
+        double y = (double) rand() / RAND_MAX;
+        double distance = x * x + y * y;
+        if (distance <= 1){
+	   local_cnt++;
+        }
+    }
 
     if (world_rank > 0)
     {
         // TODO: MPI workers
+	int dest = 0;
+	MPI_Request req;
+	MPI_Isend(&local_cnt, 1, MPI_UNSIGNED_LONG, dest, tag, MPI_COMM_WORLD, &req);
     }
     else if (world_rank == 0)
     {
         // TODO: non-blocking MPI communication.
         // Use MPI_Irecv, MPI_Wait or MPI_Waitall.
-        MPI_Request requests[];
-
-        MPI_Waitall();
+	int num_request = world_size - 1;
+	MPI_Status status[num_request];
+        MPI_Request requests[num_request];
+	long long int buf[num_request];
+	for (int source = 1; source < world_size; source++){
+	    MPI_Irecv(&buf[source-1], 1, MPI_UNSIGNED_LONG, source, tag, MPI_COMM_WORLD, &requests[source-1]);
+	}
+        MPI_Waitall(num_request, requests, status);
+	for (int i = 0; i < num_request; i++){
+	    local_cnt += buf[i];
+	}
     }
 
     if (world_rank == 0)
     {
         // TODO: PI result
+	count = local_cnt;
+	pi_result = 4 * count / (double) tosses;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
