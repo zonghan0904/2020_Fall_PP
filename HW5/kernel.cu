90fccad6e7ab62e2a56fad60a5a627d579bce06d
zonghan <steven.me05@nctu.edu.tw>
2020-12-19 16:41:40 +0800

    [HW5] init

---
+++ HW5/kernel.cu
@@ -0,0 +1,17 @@
+#include <cuda.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+__global__ void mandelKernel() {
+    // To avoid error caused by the floating number, use the following pseudo code
+    //
+    // float x = lowerX + thisX * stepX;
+    // float y = lowerY + thisY * stepY;
+}
+
+// Host front-end function that allocates the memory and launches the GPU kernel
+void hostFE (float upperX, float upperY, float lowerX, float lowerY, int* img, int resX, int resY, int maxIterations)
+{
+    float stepX = (upperX - lowerX) / resX;
+    float stepY = (upperY - lowerY) / resY;
+}
---
+++ HW5/kernel.h
@@ -0,0 +1,7 @@
+#ifndef KERNEL_H_
+#define KERNEL_H_
+
+//extern "C"
+void hostFE(float uX, float uY, float lX, float lY, int *image, int resX, int resY, int maxIterations);
+
+#endif /* KERNEL_H_ */
