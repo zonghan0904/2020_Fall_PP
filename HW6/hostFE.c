8fc70431336df6d50f7ce01114cbd48fb479ee10
zonghan <steven.me05@nctu.edu.tw>
2021-01-02 07:25:33 +0000

    [HW6] init

---
+++ HW6/hostFE.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hostFE.h"
+#include "helper.h"
+
+void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
+            float *inputImage, float *outputImage, cl_device_id *device,
+            cl_context *context, cl_program *program)
+{
+    cl_int status;
+    int filterSize = filterWidth * filterWidth;
+
+}
