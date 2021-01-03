6ef3a152ec4470168c94bfb1f28e5cd09437ee9b
zonghan <steven.me05@nctu.edu.tw>
2021-01-03 16:44:28 +0000

    [HW6] implement opencl zero-padding convolution kernel

--- HW6/hostFE.c
+++ HW6/hostFE.c
@@ -8,6 +8,52 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
             cl_context *context, cl_program *program)
 {
     cl_int status;
-    int filterSize = filterWidth * filterWidth;
+    int filterSize = filterWidth * filterWidth * sizeof(float);
+    int mem_size = imageHeight * imageWidth * sizeof(float);
 
+    // create command queue
+    cl_command_queue myqueue;
+    myqueue = clCreateCommandQueue(*context, *device, 0, &status);
+
+    // allocate device memory
+    cl_mem d_filter = clCreateBuffer(*context, CL_MEM_READ_ONLY, filterSize, NULL, &status);
+    cl_mem d_inputImage = clCreateBuffer(*context, CL_MEM_READ_ONLY, mem_size, NULL, &status);
+    cl_mem d_outputImage = clCreateBuffer(*context, CL_MEM_WRITE_ONLY, mem_size, NULL, &status);
+
+    // copy data from host memory to device memory
+    status = clEnqueueWriteBuffer(myqueue, d_filter, CL_TRUE, 0, filterSize, (void *)filter, 0, NULL, NULL);
+    CHECK(status, "clEnqueueWriteBuffer");
+    status = clEnqueueWriteBuffer(myqueue, d_inputImage, CL_TRUE, 0, mem_size, (void *)inputImage, 0, NULL, NULL);
+    CHECK(status, "clEnqueueWriteBuffer");
+
+    // create kernel function
+    cl_kernel mykernel = clCreateKernel(*program, "convolution", status);
+    CHECK(status, "clCreateKernel");
+
+    // set kernel function args
+    clSetKernelArg(mykernel, 0, sizeof(cl_int), (void *)&filterWidth);
+    clSetKernelArg(mykernel, 1, sizeof(cl_mem), (void *)&d_filter);
+    clSetKernelArg(mykernel, 2, sizeof(cl_int), (void *)&imageHeight);
+    clSetKernelArg(mykernel, 3, sizeof(cl_int), (void *)&imageWidth);
+    clSetKernelArg(mykernel, 4, sizeof(cl_mem), (void *)&d_inputImage);
+    clSetKernelArg(mykernel, 5, sizeof(cl_mem), (void *)&d_outputImage);
+
+    // workgroups parameter
+    size_t localws[2] = {25, 25};
+    size_t globalws[2] = {imageWidth, imageHeight};
+
+    // execute kernel function
+    status = clEnqueueNDRangeKernel(myqueue, mykernel, 2, 0, globalws, localws, 0, NULL, NULL);
+    CHECK(status, "clEnqueueNDRangeKernel");
+
+    // copy data from device memory to host memory
+    status = clEnqueueReadBuffer(myqueue, d_outputImage, CL_TRUE, 0, mem_size, (void *)outputImage, NULL, NULL, NULL);
+    CHECK(status, "clEnqueueReadBuffer");
+
+    // release opencl object
+    status = clReleaseCommandQueue(myqueue);
+    status = clReleaseMemObject(d_filter);
+    status = clReleaseMemObject(d_inputImage);
+    status = clReleaseMemObject(d_outputImage);
+    status = clReleaseKernel(mykernel);
 }
--- HW6/kernel.cl
+++ HW6/kernel.cl
@@ -1,4 +1,24 @@
-__kernel void convolution() 
+__kernel void convolution(int filterWidth, __global float * filter,
+			  int imageHeight, int imageWidth,
+			  __global float * inputImage, __global float * outputImage)
 {
-   
+    int halffilterSize = filterWidth / 2;
+    float sum;
+    int k, l;
+    int ix = get_global_id(0);
+    int iy = get_global_id(1);
+
+    sum = 0;
+
+    for (k = -halffilterSize; k <= halffilterSize; k++){
+	for (l = -halffilterSize; l <= halffilterSize; l++){
+	    if (iy + k >= 0 && iy + k < imageHeight &&
+		ix + l >= 0 && ix + l < imageWidth){
+                sum += inputImage[(iy + k) * imageWidth + ix + l] *
+                       filter[(k + halffilterSize) * filterWidth +
+                              l + halffilterSize];
+	    }
+	}
+    }
+    outputImage[iy * imageWidth + ix] = sum;
 }
